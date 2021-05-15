5fa755baccf8eda329e43c39588346627e6f4efe
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2021-05-15 15:10:54 +0300

    Refactor API for async support

--- app/main.py
+++ app/main.py
@@ -1,24 +1,38 @@
-import time
-
-from fastapi import Depends, FastAPI, HTTPException
-from starlette.requests import Request
-import redis
-import uuid
+import asyncio
 import json
-from schemas import (ZSLTextInput, SingleTextInput,
-                     QAInput)
-
 import logging
-queue = redis.StrictRedis(host="redis")
+import time
+import uuid
+
+import aioredis
+import async_timeout
+from fastapi import Depends, FastAPI, HTTPException
 from qdrant_client import QdrantClient
-qdrant = QdrantClient(host="qdrant", port=6333)
-logging.warning("Connected to qdrant")
+from starlette.requests import Request
+
+from schemas import QAInput, SingleTextInput, ZSLTextInput
 
 app = FastAPI(
     title="Intelligence as a Service",
     description="Fast, easy-to-use, consistent AI APIs APIs for developers. Use one of our SDKs or simply make an http request to our endpoints from anywhere you need it.",
     version="0.0.1-alpha"
 )
+
+
+@app.on_event('startup')
+async def app_startup():
+    app.state.redis = await aioredis.create_redis_pool('redis://redis:6379/0?encoding=utf-8')
+    logging.warning('Connected to redis')
+    app.state.qdrant = QdrantClient(host="qdrant", port=6333)
+    logging.warning('connected to Qdrant')
+
+
+@app.on_event('shutdown')
+async def app_shutdown():
+    app.state.redis.close()
+    await app.state.redis.wait_closed()
+    app.state.qdrant.close()
+
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
@@ -44,23 +58,24 @@ async def classify_text(zsl_input: ZSLTextInput):
     k = str(uuid.uuid4())
     zsl_input = zsl_input.dict()
     zsl_input["id"] = k
-    queue.rpush("zsl", json.dumps(zsl_input))
-    num_tries, max_tries = 0, 20
-    while num_tries < max_tries:
-        num_tries += 1
-        output = queue.get(k)
-        if output is not None:
-            resp["predictions"] = json.loads(output)
-            queue.delete(k)
-            break
-        time.sleep(0.08)
-        resp["success"] = True
-    else:
-        raise HTTPException(status_code=400, detail=f"Request failed after {max_tries} tries")
+    await app.state.redis.rpush("zsl", json.dumps(zsl_input))
+    try:
+        async with async_timeout.timeout(3.0):
+            while True:
+                output = await app.state.redis.get(k)
+                if output is not None:
+                    resp["predictions"] = json.loads(output)
+                    await app.state.redis.delete(k)
+                    break
+                asyncio.sleep(0.1)
+    except asyncio.TimeoutError:
+        raise HTTPException(
+            status_code=408, detail="Request timed out after waiting for 3 seconds")
 
+    resp["success"] = True
     return resp
 
-    
+
 @app.post('/text/summarization')
 async def summarize_text(ats_input: SingleTextInput):
     """
@@ -70,23 +85,24 @@ async def summarize_text(ats_input: SingleTextInput):
     k = str(uuid.uuid4())
     ats_input = ats_input.dict()
     ats_input["id"] = k
-    queue.rpush("ats", json.dumps(ats_input))
-    num_tries, max_tries = 0, 100
-    while num_tries < max_tries:
-        num_tries += 1
-        output = queue.get(k)
-        if output is not None:
-            resp["predictions"] = json.loads(output)
-            queue.delete(k)
-            break
-        time.sleep(0.1)
-        resp["success"] = True
-    else:
-        raise HTTPException(status_code=400, detail=f"Request failed after {max_tries} tries")
+    await app.state.redis.rpush("ats", json.dumps(ats_input))
+    try:
+        async with async_timeout.timeout(10.0):
+            while True:
+                output = await app.state.redis.get(k)
+                if output is not None:
+                    resp["predictions"] = json.loads(output)
+                    await app.state.redis.delete(k)
+                    break
+                asyncio.sleep(0.2)
+    except asyncio.TimeoutError:
+        raise HTTPException(
+            status_code=408, detail="Request timed out after waiting for 10 seconds")
 
+    resp["success"] = True
     return resp
 
-    
+
 @app.post('/text/qg')
 async def generate_questions(qg_input: SingleTextInput):
     """
@@ -96,21 +112,22 @@ async def generate_questions(qg_input: SingleTextInput):
     k = str(uuid.uuid4())
     qg_input = qg_input.dict()
     qg_input["id"] = k
-    queue.rpush("qaqg", json.dumps(qg_input))
-    num_tries, max_tries = 0, 300
-    while num_tries < max_tries:
-        num_tries += 1
-        output = queue.get(k)
-        if output is not None:
-            resp["predictions"] = json.loads(output)
-            queue.delete(k)
-            break
-        time.sleep(0.2)
-        resp["success"] = True
-    else:
-        raise HTTPException(status_code=400, detail=f"Request failed after {max_tries} tries")
+    await app.state.redis.rpush("qaqg", json.dumps(qg_input))
+    try:
+        async with async_timeout.timeout(60.0):
+            while True:
+                output = await app.state.redis.get(k)
+                if output is not None:
+                    resp["predictions"] = json.loads(output)
+                    await app.state.redis.delete(k)
+                    break
+                asyncio.sleep(0.2)
+    except asyncio.TimeoutError:
+        raise HTTPException(
+            status_code=408, detail="Request timed out after waiting for 60 seconds")
 
-    return resp    
+    resp["success"] = True
+    return resp
 
 
 @app.post('/text/qa')
@@ -122,23 +139,24 @@ async def answer_question(qa_input: QAInput):
     k = str(uuid.uuid4())
     qa_input = qa_input.dict()
     qa_input["id"] = k
-    queue.rpush("qaqg", json.dumps(qa_input))
-    num_tries, max_tries = 0, 100
-    while num_tries < max_tries:
-        num_tries += 1
-        output = queue.get(k)
-        if output is not None:
-            resp["predictions"] = json.loads(output)
-            queue.delete(k)
-            break
-        time.sleep(0.1)
-        resp["success"] = True
-    else:
-        raise HTTPException(status_code=400, detail=f"Request failed after {max_tries} tries")
+    await app.state.redis.rpush("qaqg", json.dumps(qa_input))
+    try:
+        async with async_timeout.timeout(10.0):
+            while True:
+                output = await app.state.redis.get(k)
+                if output is not None:
+                    resp["predictions"] = json.loads(output)
+                    await app.state.redis.delete(k)
+                    break
+                asyncio.sleep(0.1)
+    except asyncio.TimeoutError:
+        raise HTTPException(
+            status_code=408, detail="Request timed out after waiting for 10 seconds")
 
+    resp["success"] = True
     return resp
 
-        
+
 @app.post('/text/ner')
 async def recognize_named_entities(ner_input: SingleTextInput):
     """
@@ -148,19 +166,19 @@ async def recognize_named_entities(ner_input: SingleTextInput):
     k = str(uuid.uuid4())
     ner_input = ner_input.dict()
     ner_input["id"] = k
-    queue.rpush("ner", json.dumps(ner_input))
-    num_tries, max_tries = 0, 20
-    while num_tries < max_tries:
-        num_tries += 1
-        output = queue.get(k)
-        if output is not None:
-            resp["predictions"] = json.loads(output)
-            queue.delete(k)
-            break
-        time.sleep(0.08)
-        resp["success"] = True
-    else:
-        raise HTTPException(status_code=400, detail=f"Request failed after {max_tries} tries")
+    await app.state.redis.rpush("ner", json.dumps(ner_input))
+    try:
+        async with async_timeout.timeout(3.0):
+            while True:
+                output = await app.state.redis.get(k)
+                if output is not None:
+                    resp["predictions"] = json.loads(output)
+                    await app.state.redis.delete(k)
+                    break
+                asyncio.sleep(0.1)
+    except asyncio.TimeoutError:
+        raise HTTPException(
+            status_code=408, detail="Request timed out after waiting for 3 seconds")
 
-    return resp    
-
+    resp["success"] = True
+    return resp
